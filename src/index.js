ab26b66909f20793bcd62a3da3a6595d7500056c
priliapuspita <priliapuspita04@gmail.com>
2026-02-25 04:47:47 +0000

    new

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 function hello(nama, umur) {
     return 'Selamat Datang ' + nama + ' adalah = ' + umur + ' di PWL';
 }
-const mahasiswa = "Mingyu";
+const mahasiswa = "Prilia";
 const umur = 19;
 console.log(hello(mahasiswa, umur));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 function hello(nama: string, umur: number): string{
     return 'Selamat Datang '+nama+' adalah = '+umur+' di PWL';
 }
-const mahasiswa = "Mingyu";
+const mahasiswa = "Prilia";
 const umur:number = 19;
 console.log(hello(mahasiswa,umur));
